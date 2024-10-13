d53fbd899f9432946b51d89497ab65af4e0b0b22
Jardel Weyrich <jweyrich@gmail.com>
2024-10-13 15:54:09 -0300

    Also print AST repr with --debug-ast

--- query/parser.py
+++ query/parser.py
@@ -35,18 +35,26 @@ class Parser(object):
             self._parse_results = parse_results
             return self._parse_results
 
-    def dump_ast(self, node, tab=""):
+    def dump_ast(self, node: Node):
         """
         Dump the AST to the console.
+        """
+        print(f"AST repr:\n{node}", file=sys.stderr)
+        print("AST tree:", file=sys.stderr)
+        self._dump_ast_node_recursively(node)
+
+    def _dump_ast_node_recursively(self, node, tab=""):
+        """
+        Recursively dump the AST nodes to the console.
         """
         text = f"('{node}')" if isinstance(node, str | float | int) else ""
         print(tab + "┗━ " + str(node.__class__.__name__) + text, file=sys.stderr)
         if isinstance(node, Node):
             for child in node.children:
-                self.dump_ast(child, tab + "   ")
+                self._dump_ast_node_recursively(child, tab + "   ")
         elif isinstance(node, pp.ParseResults):
             for child in node:
-                self.dump_ast(child, tab + "   ")
+                self._dump_ast_node_recursively(child, tab + "   ")
         elif isinstance(node, list):
             for child in node:
-                self.dump_ast(child, tab + "   ")
+                self._dump_ast_node_recursively(child, tab + "   ")
